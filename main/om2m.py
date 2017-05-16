8a9c35c09ed94c0cf0555d627499dac1464b86ec
oHSUANo <chhu0830@gmail.com>
2017-05-16 18:54:09 +0800

    Modify om2m to functions

--- main/om2m.py
+++ main/om2m.py
@@ -1,20 +1,14 @@
 import requests
 import sys
 
-if len(sys.argv) == 1:
-    print('usage: python3 om2m.py 2 <application>')
-    print('usage: python3 om2m.py 3 <application> <container>')
-    print('usage: python3 om2m.py 4 <application> <container> <data>')
+server = 'http://140.113.66.98:8080'
 
-elif sys.argv[1] == '2':
-    if len(sys.argv) != 3:
-        print('usage: python3 om2m.py 2 <application>')
-        sys.exit()
-    url = 'http://140.113.66.98:8080/~/in-cse'
+def create_application(app_name):
+    url = server + '/~/in-cse'
     headers={
         'X-M2M-Origin': 'admin:admin',
         'Content-Type': 'application/xml;ty=2',
-        'X-M2M-NM': sys.argv[2]
+        'X-M2M-NM': app_name
     }
     data='<om2m:ae xmlns:om2m="http://www.onem2m.org/xml/protocols">\
               <api>app-sensor</api>\
@@ -23,29 +17,19 @@ elif sys.argv[1] == '2':
           </om2m:ae>'
     print(requests.post(url, headers=headers, data=data))
 
-elif sys.argv[1] == '3':
-    if len(sys.argv) != 4:
-        print('usage: python3 om2m.py 3 <application> <container>')
-        sys.exit()
-    application = sys.argv[2]
-    container = sys.argv[3]
-    url = 'http://140.113.66.98:8080/~/in-cse/in-name/' + application
+def create_container(app_name, con_name):
+    url = server + '/~/in-cse/in-name/' + app_name
     headers={
         'X-M2M-Origin': 'admin:admin',
         'Content-Type': 'application/xml;ty=3',
-        'X-M2M-NM': container
+        'X-M2M-NM': con_name
     }
     data='<om2m:cnt xmlns:om2m="http://www.onem2m.org/xml/protocols">\
           </om2m:cnt>'
     print(requests.post(url, headers=headers, data=data))
 
-elif sys.argv[1] == '4':
-    if len(sys.argv) != 5:
-        print('usage: python3 om2m.py 4 <application> <container> <data>')
-        sys.exit()
-    application = sys.argv[2]
-    container = sys.argv[3]
-    url = 'http://140.113.66.98:8080/~/in-cse/in-name/' + application + '/' +  container
+def create_contain_instance(app_name, con_name, con_ins_data):
+    url = server + '/~/in-cse/in-name/' + app_name + '/' +  con_name
     headers={
         'X-M2M-Origin': 'admin:admin',
         'Content-Type': 'application/xml;ty=4',
@@ -55,5 +39,17 @@ elif sys.argv[1] == '4':
               <con>\
                   %s\
               </con>\
-          </om2m:cin>' % sys.argv[4]
+          </om2m:cin>' % con_ins_data
     print(requests.post(url, headers=headers, data=data))
+
+
+if __name__ == '__main__':
+    print('usage: python3 om2m.py 2 <application>')
+    print('usage: python3 om2m.py 3 <application> <container>')
+    print('usage: python3 om2m.py 4 <application> <container> <data>')
+    if sys.argv[1] == '2':
+        create_application(sys.argv[2])
+    elif sys.argv[1] == '3':
+        create_container(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == '4':
+        create_contain_instance(sys.argv[2], sys.argv[3], sys.argv[4])
