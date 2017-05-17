eedcc1e291136107b40ed3dc36c8ca66e2c8dd53
oHSUANo <chhu0830@gmail.com>
2017-05-18 05:20:00 +0800

    Modify usage of sonic.py

--- sensors/sonic.py
+++ sensors/sonic.py
@@ -3,32 +3,12 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-if (len(sys.argv) != 2):
-    print("usage:", sys.argv[0], "<IPaddress>")
-    sys.exit()
-
 trigger_pin_2 = 23
 echo_pin_2 = 24
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(trigger_pin_2, GPIO.OUT)
 GPIO.setup(echo_pin_2, GPIO.IN)
-def post(distance): 
-    import requests
-    from datetime import datetime
-    from uuid import getnode as get_mac
-
-    url = 'http://' + sys.argv[1] + '/data'
-    now = datetime.now()
-
-    payload = {
-        'datum[mac]': get_mac(),
-        'datum[speed]': now.second,
-        'datum[distance]': distance
-    }
-
-    r = requests.post(url, data=payload)
-    return
 
 def send_trigger_pulse(pin):
     GPIO.output(pin, True)
@@ -49,10 +29,9 @@ def get_distance():
     pulse_len_2 = finish - start
 
     distance_cm_2 = pulse_len_2 * 340 * 100 / 2
-    # distance_in = distance_cm / 2.5
     return distance_cm_2
 
-while True:
-    post(get_distance())
-    print("cm=%f" % get_distance())
-    time.sleep(1)
+if __name__ == '__main__':
+    while True:
+        print("cm=%f" % get_distance())
+        time.sleep(1)
