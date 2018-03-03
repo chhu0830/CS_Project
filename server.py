84aab626699a2f3dd96f7cffdf28c9d5487b0804
oHSUANo <chhu0830@gmail.com>
2018-03-03 12:07:46 +0800

    Revise server strategy

--- server.py
+++ server.py
@@ -6,54 +6,33 @@ import time
 from utils.om2m import get_data, create_content_instance
 
 data = {}
-danger_distance = 20.0
+danger_distance = 100.0
 danger_speed = 100.0
-low_speed = 10.0
+low_speed = 60.0
 
 while True:
     string = input()
-    start = string.find('<con>')
-    end = string.find('</con>')
+    start = string.find("<con>")
+    end = string.find("</con>")
 
     if start != -1:
         datum = string[start+5:end]
-        datum = datum.strip().split(' ')
+        datum = datum.strip().split(" ")
     else:
         continue
 
-    print(datum)
     mac, cur_speed, cur_distance = datum
-
     data[mac] = [cur_speed, cur_distance]
+    min_speed = min(float(d[0]) for mac, d in data.items())
 
-    max_speed = 0.0
-    command = ''
-
+    command = "KEEP"
     if float(cur_distance) < danger_distance:
-        command = 'SLOW'
-        print('SLOW')
+        command = "SLOW"
     elif float(cur_speed) > danger_speed:
-        command = 'SLOW'
-        print('SLOW')
-    elif float(cur_speed) < max_speed:
-        command = 'FAST'
-        print('FAST')
+        command = "SLOW"
+    elif float(cur_speed) <= min_speed:
+        command = "FAST"
     elif float(cur_speed) < low_speed:
-        command = 'FAST'
-        print('FAST')
-    else:
-        command = 'KEEP'
-        print('KEEP')
-    print(create_content_instance('COMMAND', mac, command))
+        command = "FAST"
 
-'''
-    for i in data:
-        if float(data[i][0]) > max_speed:
-            max_speed = float(data[i][0])
-'''
-
-'''
-    f = open('log/' + recv[0], 'a')
-    f.write(str(datetime.datetime.now()) + '\t' + cur_speed + '\t' + cur_distance + '\t' + msg + '\n')
-    f.close()
-'''
+    print(datum, min_speed, command, create_content_instance("COMMAND", mac, command))
